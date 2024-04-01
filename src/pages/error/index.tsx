8f530d9cadfd4564ede3a4ea0a5d85544d7380ac
Vũ Ngọc Thanh Trúc <vutruc0702@gmail.com>
2024-04-01 23:11:44 +0700

    [Truc/frontend]: page error 404

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -9,12 +9,12 @@ const Error = () =>{
                     <div className="text-center">
                     <h2 className="d-flex justify-content-center align-items-center gap-2 mb-4">
                         <span className="display-1 fw-bold">4</span>
-                        <i className="bi bi-exclamation-circle-fill text-danger display-4"></i>
+                        <i className="bi bi-exclamation-circle-fill text-danger display-4">0</i>
                         <span className="display-1 fw-bold bsb-flip-h">4</span>
                     </h2>
                     <h3 className="h2 mb-2">Oops! </h3>
                     <p className="mb-5">Trang không tồn tại.</p>
-                    <a className="btn bsb-btn-5xl btn-dark rounded-pill px-5 fs-6 m-0" href="#!" role="button">Quay lại trang chủ</a>
+                    <a className="btn bsb-btn-5xl btn-dark rounded-pill px-5 fs-6 m-0" href="/" role="button">Quay lại trang chủ</a>
                     </div>
                 </div>
                 </div>
