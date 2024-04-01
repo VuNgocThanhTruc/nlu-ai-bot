41956344e10343cd3b20f93d1f9430d7a027ee27
Vũ Ngọc Thanh Trúc <vutruc0702@gmail.com>
2024-04-01 23:10:20 +0700

    [Truc/frontend]: create page error 404

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Error = () =>{
-    return <div>ERROR 404</div>
+    return (
+        <section className="py-3 py-md-5 min-vh-100 d-flex justify-content-center align-items-center">
+            <div className="container">
+                <div className="row">
+                <div className="col-12">
+                    <div className="text-center">
+                    <h2 className="d-flex justify-content-center align-items-center gap-2 mb-4">
+                        <span className="display-1 fw-bold">4</span>
+                        <i className="bi bi-exclamation-circle-fill text-danger display-4"></i>
+                        <span className="display-1 fw-bold bsb-flip-h">4</span>
+                    </h2>
+                    <h3 className="h2 mb-2">Oops! </h3>
+                    <p className="mb-5">Trang không tồn tại.</p>
+                    <a className="btn bsb-btn-5xl btn-dark rounded-pill px-5 fs-6 m-0" href="#!" role="button">Quay lại trang chủ</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Error;
