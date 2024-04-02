578c4749066f39d9dd338eb70b4602960371c3e1
Vũ Ngọc Thanh Trúc <vutruc0702@gmail.com>
2024-04-02 22:09:04 +0700

    [Truc/frontend]: split section page home & create UI Header of section main chat

---
+++ src/components/main/header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () =>{
+    return (
+        <div className="mt-2 ">
+            <h6>Generate a Python hooks useLocalStorage</h6>
+            <div>8 dialogues</div>
+            <hr />
+        </div>
+    )
+}
+
+export default Header;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import MainChat from "../../components/main/mainChat";
+import SideBar from "../../components/sidebar";
 
 const Home = () =>{
-    return <div>Page Home</div>
+    return (
+        <div className="row">
+            <div className="col-2">
+                <SideBar/>  
+            </div>
+            <div className="col-10">
+                <MainChat/>
+            </div>
+        </div>
+    )
 }
 
 export default Home;
