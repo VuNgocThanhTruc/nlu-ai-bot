41cad6a5602f75d9f9c0be36d51985a4ca567af6
Vũ Ngọc Thanh Trúc <vutruc0702@gmail.com>
2024-06-12 16:25:13 +0700

    (Truc/frontend): done main chat footer

--- src/components/main/componentChat/ComponentChat.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ComponentChat = () =>{
-    return (
-        <div className="">Component Chat</div>
-    )
-}
-
-export default ComponentChat;
--- src/components/main/listChat/index.tsx
+++ src/components/main/listChat/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import NLUAILogo from '../../../assests/logo-nlu-ai.png'
+import userLogo from '../../../assests/user.svg'
+import './style.css'
 
 const ListChat = () =>{
     return (
-        <div className="flex-grow-1">List Chat</div>
+        <div className="list-chat flex-grow-1">
+            <div className="chat bot">
+                <img className="logo" src={NLUAILogo} alt="" />
+                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eaque labore facere, officia mollitia exercitationem minima animi similique magni voluptate dignissimos laborum delectus, corrupti ea. Asperiores vel labore unde ullam!</p>
+            </div>
+
+            <div className="chat user">
+                <img className="logo" src={userLogo} alt="" />
+                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eaque labore facere, officia mollitia exercitationem minima animi similique magni voluptate dignissimos laborum delectus, corrupti ea. Asperiores vel labore unde ullam!</p>
+            </div>
+        </div>
     )
 }
 
