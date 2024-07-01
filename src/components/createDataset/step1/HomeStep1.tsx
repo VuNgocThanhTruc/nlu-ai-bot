ffbe1f440006271122f1df1c10df4b07c90550b1
Nguyen Dang Phuoc <nguyenphuoc04112002@gmail.com>
2024-07-01 10:09:04 +0700

    [NguyenDangPhuoc] update side and create page for create dataset

---
+++ src/components/createDataset/step1/HomeStep1.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeStep1: React.FC = () => {
+    return (
+        <div>
+            <h2>Screen 1</h2>
+            {/* Add more content for Screen 1 here */}
+        </div>
+    );
+};
+
+export default HomeStep1;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import MainChat from "../../components/main/mainChat";
 import Sidebar from "../../components/sidebar/Sidebar";
+import CreateDataset from "../../components/createDataset/HomeCreateDataset";
 
-const Home = () =>{
+const Home = () => {
+    const [showMainChat, setShowMainChat] = useState(true);
+
+    const toggleComponent = () => {
+        setShowMainChat(!showMainChat);
+    };
+
     return (
-        <div className="row">
-            <div className="col-2">
-                <Sidebar/>
+        <div className="row" style={{ height: "100vh" }}>
+            <div className="col-3">
+                <Sidebar onToggleComponent={toggleComponent} />
             </div>
-            <div className="col-10">
-                <MainChat/>
+            <div className="col-9">
+                {showMainChat ? <MainChat /> : <CreateDataset />}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
