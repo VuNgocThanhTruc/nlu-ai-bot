28f8904e65776b7f735441021a17a25bec911858
Vũ Ngọc Thanh Trúc <vutruc0702@gmail.com>
2024-07-07 18:07:19 +0700

    [Truc]: test deploy again 1

--- src/utils/server_util.ts
+++ src/utils/server_util.ts
@@ -1,5 +1,5 @@
 export const API_FASTAPI = {
    url_deploy: 'http://localhost:8000',
-   url: 'https://e3e7-34-81-9-184.ngrok-free.app',
+   url: 'https://8762-34-81-9-184.ngrok-free.app',
    ws_server: 'ws://6.tcp.ngrok.io:10648',
 }
