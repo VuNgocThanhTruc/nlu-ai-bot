15bd5e57943e17c3a0e4c86c2d60acaf96aa6759
Vũ Ngọc Thanh Trúc <vutruc0702@gmail.com>
2024-08-06 09:26:53 +0700

    [Truc]: fix add chats in db

--- src/components/main/chatFooter/index.tsx
+++ src/components/main/chatFooter/index.tsx
@@ -3,13 +3,11 @@ import sendBtn from '../../../assests/send-icon.svg'
 import "./style.css";
 import { useDispatch, useSelector } from "react-redux";
 import { loadingResponseChatSlice } from "../../../redux/slices/loadingResponseChatSlice";
-import { ChatProps } from "../../../utils/types";
+import { ChatProps, ListChatProps } from "../../../utils/types";
 import { chatsSlice } from "../../../redux/slices/chatsSlice";
 import { FETCH_POST_ROOM, POST_CHAT } from "../../../utils/FetchData";
 import { USER_INFO } from "../../../mock-data/mockData";
-import { chats, roomsSelector, roomsselectedSelector } from "../../../redux/selectors";
-import { roomsSlice } from "../../../redux/slices/roomsSlice";
-import axios from "axios";
+import { chats, roomsselectedSelector } from "../../../redux/selectors";
 
 const ChatFooter = () => {
     const [text, setText] = useState('')
@@ -17,12 +15,13 @@ const ChatFooter = () => {
     const [isTextEmpty, setIsTextEmpty] = useState(true);
     const ws = useRef<WebSocket | null>(null);
     const roomsselected = useSelector(roomsselectedSelector);
-    const roomsFromStore = useSelector(roomsSelector);
     const roomsselectedRef = useRef(roomsselected);
-    const textRef = useRef(text); 
+    const textRef = useRef(text);
+    const chatsFromStore = useSelector(chats);
+    let sentense_generate = ''
 
     useEffect(() => {
-        textRef.current = text; 
+        textRef.current = text;
     }, [text]);
 
     useEffect(() => {
@@ -32,6 +31,7 @@ const ChatFooter = () => {
     useEffect(() => {
         const websocketUrl = process.env.REACT_APP_WEBSOCKET_URL;
         if (websocketUrl) {
+
             ws.current = new WebSocket(websocketUrl);
             ws.current.onopen = () => {
                 console.log('WebSocket is open now.');
@@ -42,23 +42,44 @@ const ChatFooter = () => {
             };
 
             ws.current.onmessage = (event) => {
+                setIsTextEmpty(true)
                 const response = JSON.parse(event.data);
-                // console.log(response);
-                
-                if (response.user.role != 0)
+                let dataRoom: ChatProps = {
+                    "user": USER_INFO,
+                    "text": ''
+                }
+
+                if (response.user.role != 0) {
+                    sentense_generate = ''
+                    dataRoom = {
+                        "user": USER_INFO,
+                        "text": textRef.current
+                    }
                     dispatch(chatsSlice.actions.addChat(response));
+
+                    if (chats.length === 0) {
+                        dataRoom.text = textRef.current;
+                        FETCH_POST_ROOM("/rooms/", dataRoom, dispatch);
+                    }
+                    POST_CHAT(dataRoom, roomsselectedRef.current, dispatch)
+                }
                 else {
                     dispatch(loadingResponseChatSlice.actions.setLoadingResponse(false))
                     const { user, generate_text, prev_text, stop } = response;
                     const newChat: ChatProps = {
                         user: user,
-                        text: ""
+                        text: ''
                     };
                     prev_text == textRef.current && dispatch(chatsSlice.actions.addChat(newChat));
                     dispatch(chatsSlice.actions.updateLastChat(generate_text));
+                    sentense_generate += generate_text
                     if (stop) {
+                        dataRoom = {
+                            "user": user,
+                            "text": sentense_generate
+                        }
+                        POST_CHAT(dataRoom, roomsselectedRef.current, dispatch)
                         setText('')
-                        setIsTextEmpty(true)
                     }
                 }
             };
@@ -78,43 +99,8 @@ const ChatFooter = () => {
         if (text.trim() !== '' && ws.current && ws.current.readyState === WebSocket.OPEN) {
             // Send chat to WebSocket server
             ws.current?.send(JSON.stringify(newChat));
-            let dataRoom = {
-                "id_user": USER_INFO.id,
-                "title": text
-            }
-            
-            const data = { "input": newChat.text }
+
             dispatch(loadingResponseChatSlice.actions.setLoadingResponse(true))
-            // setTimeout(() => {
-            //     const newBotChat: ChatProps = {
-            //         user: { id: 1, role: 0, username: "bot" },
-            //         text: "Đã tạo sinh văn bản thành công!"
-            //     }
-            //     ws.current?.send(JSON.stringify(newBotChat));
-            //     dispatch(loadingResponseChatSlice.actions.setLoadingResponse(false))
-            // }, 3000);
-            // try {
-            //     const response = await axios.post(`${process.env.REACT_APP_URL_SERVER}api/generate`, data);
-
-            //     if (response.data.status === 200) {
-            //         console.log("chats.length: " + chats.length);
-
-            //         chats.length == 0 && FETCH_POST_ROOM("/rooms/", dataRoom, dispatch)
-            //         dispatch(loadingResponseChatSlice.actions.setLoadingResponse(false))
-            //         const newBotChat: ChatProps = {
-            //             user: { id: 0, role: 0, username: "bot" },
-            //             text: response.data.data[0].generated_text
-            //         }
-            //         // Send bot response to WebSocket server
-            //         ws.current?.send(JSON.stringify(newBotChat));
-            //     }
-            // } catch (error) {
-            //     if (axios.isAxiosError(error)) {
-            //         console.error('Error with axios request:', error.message);
-            //     } else {
-            //         console.error('Unexpected error:', error);
-            //     }
-            // }
         }
     }
 
