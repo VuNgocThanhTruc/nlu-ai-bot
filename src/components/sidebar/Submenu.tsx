6c5b328e66bc06eb920733a1d212db027f9ebd26
Vũ Ngọc Thanh Trúc <vutruc0702@gmail.com>
2024-08-12 15:16:21 +0700

    [Truc/deploy]: fix to deploy 1

--- src/components/sidebar/Submenu.tsx
+++ src/components/sidebar/Submenu.tsx
@@ -4,7 +4,6 @@ import { SidebarItem } from "../../model/SidebarItem";
 import {
     SidebarLink,
     SidebarLabel,
-    DropdownLink
 } from './SubmenuStyles'; // Importing the styled-components
 import { useDispatch, useSelector } from 'react-redux';
 import { chatsSlice } from '../../redux/slices/chatsSlice';
@@ -35,20 +34,11 @@ const Submenu: FC<SidebarLinkProps> = ({ item }) => {
 
     return (
         <>
-            <SidebarLink onClick={handleOnClick} className={item.idRoom == roomsselected ? 'selected' : ''}>
+            <SidebarLink onClick={handleOnClick} className={item.idRoom === roomsselected ? 'selected' : ''}>
                 {item.icon}
                 <SidebarLabel>{item.title}</SidebarLabel>
                 <div>{item?.subnav && subnav ? item?.iconOpened : item?.iconClosed}</div>
             </SidebarLink>
-            {/* {subnav &&
-                item?.subnav?.map((subnavItem, index) => {
-                    return (
-                        <DropdownLink to={subnavItem.path} key={index}>
-                            {subnavItem.icon}
-                            <SidebarLabel>{subnavItem.title}</SidebarLabel>
-                        </DropdownLink>
-                    );
-                })} */}
         </>
     );
 };
